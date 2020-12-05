148d0501349993ccfb9be401b5f66d3a4a0f8f23
Aanchalt14 <aanchalt71@gmail.com>
2020-12-05 11:01:15 +0530

    Added : Initial position initialized

---
+++ SnakeLadder.java
@@ -0,0 +1,12 @@
+public class SnakeLadder {
+  
+	// initializing starting position
+	static void start() {
+    int starting =0; 
+    System.out.println("Starting position" +starting );
+                         }
+
+	public static void main (int args []) {
+    start();
+       }
+}
