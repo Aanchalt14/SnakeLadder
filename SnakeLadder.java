44705158593207489e352b10f123703f36a1365b
Aanchalt14 <aanchalt71@gmail.com>
2020-12-05 11:48:04 +0530

    Added : get Random number between 1-6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,25 @@
 public class SnakeLadder {
   
 	// initializing starting position
-	static void start() {
+    static void start() {
     int starting =0; 
     System.out.println("Starting position" +starting );
-                         }
-
+      }
+     
+    
+    //get number between 1-6
+    static int Diceroll() {
+ 	   int max=6;
+ 	   int min=1;
+       int c=(int)Math.random()*(max-min+1)+min;
+       return c;
+      }
+    
+    
 	public static void main (int args []) {
-    start();
-       }
+     int player=1;
+     start();
+     Diceroll();
+     }
 }
+
